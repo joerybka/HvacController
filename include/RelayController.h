29dd844f58caa364977a49ea023d353376d9788e
Joe Rybka <6586029+joerybka@users.noreply.github.com>
2024-04-28 16:55:07 -0400

    document the relay methods

--- include/RelayController.h
+++ include/RelayController.h
@@ -14,18 +14,46 @@ private:
   PinController _pin;
 
 public:
+  /**
+   * Create a controller that uses the identified pin to control a relay
+   * circuit.
+   * @param pinNumber The number of the IO pin to use when controlling the relay
+   * @param isNormallyClosed  True if the relay is wired normally closed, defaults to false
+   */
   RelayController(uint8_t pinNumber, bool isNormallyClosed = false);
 
+  /**
+   * Run all initialization code, call this in setup()
+   */
   void Initialize();
 
+  /**
+   * Close the relay, controls the pin to properly set it to closed
+   */
   void Close();
 
+  /**
+   * Open the relay, controls the pin to properly set it to open
+   */
   void Open();
 
+  /**
+   * Toggle the relay to the opposite state, under the covers it toggles
+   * the pin from its current state to the opposite state.  Unless you have
+   * a fast relay, be careful calling this too much or too quickly.
+   */
   void Toggle();
 
+  /**
+   * Check if the relay is currently closed.
+   * @return True if the relay is closed, otherwise false
+   */
   bool IsClosed();
 
+  /**
+   * Check if the relay is currently open.
+   * @return True if the relay is open, otherwise false
+   */
   bool IsOpen();
 };
 
